bccfe557020ef68903b38814c834049d36d6cbca
Aya Kurokawa <kur20002@byui.edu>
2022-10-06 11:17:07 -0600

    week4

---
+++ tests/login.js
@@ -0,0 +1,13 @@
+const fetch =require('node-fetch');
+
+const login = async ()=>{
+    const tokenResponse = await fetch('https://dev.stedi.me/login',{
+  method: 'POST',
+  body:JSON.stringify({
+    userName: "scmurdock@gmail.com",
+    password:"P@ssw0rd"
+  })
+})
+};
+
+export {login};
